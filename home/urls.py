7bed6624bb4baf755c8d77c7ba3d9557cabb6eaa
Shradhey Pathak <pathak.shradhey1008@gmail.com>
2020-10-03 16:39:37 +0530

    SignUp

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  
+    path('index/', views.strange, name="strange"),
+    path('signup/', views.signup, name="signup"),
+    path('signup/verify/',views.verify, name="verify")
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def strange(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def verify(request):
+    return render(request,'verify.html')
