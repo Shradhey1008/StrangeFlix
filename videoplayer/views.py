55a6b19802e20794127159c2a729918a2f79a2d0
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2020-10-10 02:07:33 +0530

    Added videoplayer

---
+++ videoplayer/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .models import Item, Video
+from django.core.files.storage import FileSystemStorage
+from .forms import VideoForm
+# Create your views here.
+
+def video(request):
+    if request.method == 'POST':
+        url = request.POST.get('link')
+        
+        d = Item.objects.filter(video=url)
+        if d.exists() or url is None:
+            pass
+        else:
+            s = Item(video = url)
+            s.save()
+
+    obj = Item.objects.all()
+    return render(request,'video.html',{'obj':obj})
+
+def upload(request):
+
+    lastvideo= Video.objects.last()
+
+    videofile= lastvideo.videofile
+
+
+    form= VideoForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+
+        
+    # context= {'videofile': videofile,
+    #         'form': form
+    #         }
+    # context= {
+    #         'form': form
+    #         }
+        
+        
+    return render(request, 'upload.html',context)
