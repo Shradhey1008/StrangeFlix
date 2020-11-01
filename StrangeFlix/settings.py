4378f889e13409bdbd551a2707e66c441d4e1e2a
AnuragGupta <54318429+AnuragGupta806@users.noreply.github.com>
2020-11-02 01:53:11 +0530

    Update settings.py

--- StrangeFlix/settings.py
+++ StrangeFlix/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.facebook',
+    # 'allauth.socialaccount.providers.facebook',
     'crispy_forms',
     # apps
     'home',
